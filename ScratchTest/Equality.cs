867dae45e9e4a646c6ab360e753f4f5260934963
grantcolley <grantcolley@yahoo.com>
2023-01-14 22:41:44 +0000

    Update Equality.cs

--- ScratchTest/Equality.cs
+++ ScratchTest/Equality.cs
@@ -1,4 +1,5 @@
 ﻿using SratchLib;
+using System.Runtime.CompilerServices;
 
 namespace ScratchTest
 {
@@ -7,6 +8,70 @@ namespace ScratchTest
     {
         // Additional reading
         // https://stackoverflow.com/questions/371328/why-is-it-important-to-override-gethashcode-when-equals-method-is-overridden
+        //
+
+        [TestMethod]
+        public void GetHashCode_Call_Repeatedly_Returns_Same_Value()
+        {
+            // GetHashCode returns a HashCode based on the contents of the object.
+            // Calling it on the same object multiple times will return the same value.
+
+            ///        public virtual int GetHashCode()
+            ///        {
+            ///            // GetHashCode is intended to serve as a hash function for this object.
+            ///            // Based on the contents of the object, the hash function will return a suitable
+            ///            // value with a relatively random distribution over the various inputs.
+            ///            //
+            ///            // The default implementation returns the sync block index for this instance.
+            ///            // Calling it on the same object multiple times will return the same value, so
+            ///            // it will technically meet the needs of a hash function, but it's less than ideal.
+            ///            // Objects (& especially value classes) should override this method.
+            ///
+            ///            return RuntimeHelpers.GetHashCode(this);
+            ///        }
+
+            //Arrange
+            var class1 = new MyClass("Hello world!") { X = 1, Y = 2 };
+            var class2 = new MyClass("Hello world!") { X = 1, Y = 2 };
+
+            //Act
+            var class1_HashCode = class1.GetHashCode();
+
+            //Assert
+            Assert.AreEqual(class1_HashCode, class1.GetHashCode());
+        }
+
+        [TestMethod]
+        public void GetHashCode_Two_ReferenceTypes_Same_Values_Return_Different_HashCode()
+        {
+            //Arrange
+            var class1 = new MyClass("Hello world!") { X = 1, Y = 2 };
+            var class2 = new MyClass("Hello world!") { X = 1, Y = 2 };
+
+            //Assert
+            var class1_HashCode = class1.GetHashCode();
+            var class2_HashCode = class2.GetHashCode();
+
+            // Two value types with the same values return different HashCode
+            Assert.AreNotEqual(class1.GetHashCode(), class2.GetHashCode());
+        }
+
+        [TestMethod]
+        public void GetHashCode_Two_ValueTypes_Same_Values_Return_Same_HashCode()
+        {
+            //Arrange
+            var struct1 = new MyStruct("Hello world!") { X = 1, Y = 2 };
+            var struct2 = new MyStruct("Hello world!") { X = 1, Y = 2 };
+
+            //Act
+            var struct1_HashCode = struct1.GetHashCode();
+            var struct2_HashCode = struct2.GetHashCode();
+
+            //Assert
+
+            // Two value types with the same values return the same HashCode
+            Assert.AreEqual(struct1.GetHashCode(), struct2.GetHashCode());
+        }
 
         [TestMethod]
         public void Difference_Between_Boxed_Value_Types_Virtual_And_Static_Equals() 
